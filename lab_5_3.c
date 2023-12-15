9b7b5e0488298be0c07f173c383b0ba1d8475cab
talhaberkayeren <116983842+talhaberkayeren@users.noreply.github.com>
2023-12-15 20:01:05 +0300

    Update lab_5_3.c

--- lab_5_3.c
+++ lab_5_3.c
@@ -14,10 +14,12 @@ void rotateArray(int A[], int size, int index){
     }
 }
 
-void appendArrays(int array1[], int array2[],int size1, int size2, int index){
-    size2=size1 + size2;
+void appendArrays(int array1[], int array2[],int size1, int size2,int new_array[size1+size2]){
     for(int i=0; i<size1; i++){
-        array2[index + 1+ i] = array1[i]; //ikinci array in son elemanı kayboluyor
+        new_array[i]=array1[i];
+    }
+    for(int j=0; j<size2; j++){
+        new_array[size2 - 1 + j] = array2[j];
     }
 }
 
@@ -25,7 +27,7 @@ int main(){
     int rotate_index;
     int array1[]={10, 9, 8, 15, 3};
     int sizeArray1 = sizeof(array1)/sizeof(array1[0]);
-    int array2[]={2, 7, 4, 5, 11, 16};
+    int array2[]={7, 4, 16, 5, 11, 2};
     int sizeArray2 = sizeof(array2)/sizeof(array2[0]);
     printf("Array1: ");
     for(int i=0; i<sizeArray1; i++){
@@ -50,6 +52,13 @@ int main(){
         printf("%d ",array2[i]);
     }
     printf("\n");
-    
+    int newArraySize = sizeArray1 + sizeArray2;
+    int new_array[newArraySize];
+    appendArrays(array1, array2, sizeArray1, sizeArray2,new_array);
+    printf("Appended array: ");
+    for(int i=0; i<sizeArray1+sizeArray2; i++){
+        printf("%d ",new_array[i]);
+    }
+    printf("\n");
     return 0;
 }
