334ac6c20300f80e5a0f5f305f9d9f5a7caf8af9
talhaberkayeren <116983842+talhaberkayeren@users.noreply.github.com>
2024-01-07 01:57:21 +0300

    Create Find_the_Index_of_the_First_Occurrence_in_a_String.c

---
+++ Find_the_Index_of_the_First_Occurrence_in_a_String.c
@@ -0,0 +1,29 @@
+int strStr(char* haystack, char* needle) {
+    char* temp_needle = needle;
+    char* temp_haystack = haystack;
+
+    while (*temp_haystack != '\0') {
+        if (*temp_needle == *temp_haystack) {
+            char* temp_haystack_backup = temp_haystack;
+
+            while (*temp_needle != '\0') {
+                if (*temp_needle != *temp_haystack) {
+                    temp_haystack = temp_haystack_backup + 1;
+                    temp_needle = needle;
+                    break;
+                } else {
+                    temp_haystack++;
+                    temp_needle++;
+                }
+            }
+
+            if (*temp_needle == '\0') {
+                return (temp_haystack_backup - haystack);
+            }
+        } else {
+            temp_haystack++;
+        }
+    }
+
+    return -1;
+}
