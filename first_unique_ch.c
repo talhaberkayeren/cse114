6cd81a7c4a051d3e70e5a3d3fa827ddc521ce743
talhaberkayeren <116983842+talhaberkayeren@users.noreply.github.com>
2024-01-07 11:44:13 +0300

    Update first_unique_ch.c

--- first_unique_ch.c
+++ first_unique_ch.c
@@ -1,3 +1,9 @@
+/*
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+Input: s = "leetcode"  Output: 0
+Input: s = "loveleetcode"  Output: 2
+Input: s = "aabb"   Output: -1
+*/
 #include <stdio.h>
 
 int firstUniqChar(char* s) {
